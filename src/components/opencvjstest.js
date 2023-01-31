bac6628e804a4cf2e680630fc5fe4e141d083b86
SRai22 <shravansomashekara.rai@gmail.com>
2023-01-30 17:14:39 -0800

    Progress: Scalable grid based on matrix size

--- src/components/opencvjstest.js
+++ src/components/opencvjstest.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import image_1 from '../assets/forthesecrit.jpg';
 
-//similar to css to get the two cards side by side
+//similar to css to define styles
 var metaContainerStyle = {
     gridTemplateColumns: "auto auto",
     display: "grid"
@@ -14,7 +14,8 @@ var myStyleImage = {
 }
 
 var TitleStyle = {
-    color: "black"
+    color: "black",
+    fontSize: "24px"
 }
 
 var containerStyle = {
@@ -34,6 +35,8 @@ var inputSizeStyle = {
     marginTop: "10px"
 }
 
+
+
 function fromImageToCanvas(image_id,canvas_id){
     var canvas = document.getElementById(canvas_id);
     var context = canvas.getContext('2d');
@@ -43,35 +46,30 @@ function fromImageToCanvas(image_id,canvas_id){
     context.drawImage(img,0,0);
 }
 
-//Create matrix 
-function createMatrixTable(filterMatrix,setFilterMatrix){
-    let cell = []
-    console.log(filterMatrix)
-    for (var i = 0; i < Math.sqrt(filterMatrix["data"].length); i++){
-        for (var idx = 0; idx< Math.sqrt(filterMatrix["data"].length); idx++){
-            let cellID = `cell${i}-${idx}`
-            cell.push(
-            <div>
-                <input
-                    value="2"
-                />
-            </div>             
-                )
-        }
-    }
-    return cell
+function funca(e,value){
+
+    value["array"]["data"][value["position"]] = parseFloat(e.target.value)
+    value["handler"]({data: value["array"]["data"], size:value["array"]["size"]})
+
 }
 
+function handleEvent(passedInElement){
+
+    return function(e) {
+        funca(e, passedInElement)
+    }
+}
 
 
 function OpenCV({canvas_id}){
 
     const [filterMatrix, setFilterMatrix]=useState({
-        data: [-0.5,-0.5,-0.5,-0.5,4.3,-0.5,-0.5,-0.5,-0.5],size:3
+        data: [-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5],size:3
     })
     const [gridStyle, setGridStyle]=useState({
         width:"100%", height:"100%", gridTemplateColumns: "auto auto auto", display:"grid"
     })
+    const [inputStyle, setInputStyle]=useState({width:"100%", height:"100%", textAlign:"center", fontSize:"24px"})
     var image_id = 'image1'+canvas_id;
     var input_canvas_id = "canvasInputId"+ canvas_id;
     var output_canavas_id = "canvasOutputId";
@@ -79,6 +77,41 @@ function OpenCV({canvas_id}){
 
     function onLoad(){
         fromImageToCanvas(image_id,input_canvas_id);
+        setFilterMatrix({data: [-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5,-0.5],size:3});
+    }
+
+    //Create matrix 
+    function createMatrixTable(filterMatrix,setFilterMatrix){
+        let cell = []
+        console.log(filterMatrix)
+        for (var i = 0; i < Math.sqrt(filterMatrix["data"].length); i++){
+            for (var idx = 0; idx< Math.sqrt(filterMatrix["data"].length); idx++){
+                let cellID = `cell${i}-${idx}`
+                cell.push(
+                <div style = {myStyleInput} key = {cellID} id = {cellID}>
+                    <input
+                        value = {filterMatrix["data"][i*Math.sqrt(filterMatrix["data"].length)+idx]}
+                        type = "number"
+                        style= {inputStyle}
+                        onChange = {handleEvent({position: i*Math.sqrt(filterMatrix["data"].length)+idx, handler: setFilterMatrix, array: filterMatrix})}
+                    />
+                </div>             
+                )
+            }
+        }
+        return cell
+    }
+
+    const onChangeHandler = event =>{
+        let arr = []
+        let wordToRepeat = "auto ";
+        setGridStyle({width:"100%", height:"100%", display:"grid", gridTemplateColumns:`${wordToRepeat.repeat(event.target.value)}`});
+        setInputStyle({width:"100px", height:"100px", textAlign:"center", fontSize:parseInt(24*3/(event.target.value))});
+
+        for (var i=0; i<event.target.value*event.target.value; i++){
+            arr.push(0);
+        }
+        setFilterMatrix({data:arr, size:event.target.value})
     }
     return (
         <div>
@@ -98,6 +131,7 @@ function OpenCV({canvas_id}){
                             type  = "number"
                             name  = "name"
                             min   = "2"
+                            onChange={onChangeHandler}
                             value = {filterMatrix["size"]}
                             />
                         </Card.Body>
